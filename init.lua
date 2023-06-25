64362d3415490292039896098e3f8b27ef9b241b
NEsanya <nesanya1990@gmail.com>
2023-06-25 21:00:43 +0300

    Synchronise packages on install

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- If lua is not in jit mode - stop
-if not jit then 
+if not jit then
   print 'You must install lua jit, b-baka~!'
   return
 end
@@ -12,19 +12,31 @@ if pcall(function() fn.system({ 'aboba' }) end) then
 end
 
 -- Install packer if there's no packer
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim' 
-local packer_path = fn.stdpath('config')..'/plugin/packer_compiled.lua'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local packer_path = fn.stdpath('config') .. '/plugin/packer_compiled.lua'
 local is_path_empty = function(path)
   return fn.empty(fn.glob(path)) > 0
 end
 
-if is_path_empty(install_path) then
-  if is_path_empty(packer_path) then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local packer_ensure = function()
+  if not is_path_empty(install_path) then
+    return false
   end
+  if not is_path_empty(packer_path) then
+    return false
+  end
+
+  fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+  return true
 end
 
+local packer_ensure_result = packer_ensure()
+
 local require_list = { 'base', 'plugins' }
-for _,module in pairs(require_list) do
+for _, module in pairs(require_list) do
   require(module)
+end
+
+if packer_ensure_result then
+  require('packer').sync()
 end
